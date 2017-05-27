b0accef90ca23413d86b958b134f4344f8c8954f
WolfKay <haroboros@gmail.com>
2017-05-27 15:28:12 +0200

    cows and humans toggle and coexist

--- Javascript/cva.js
+++ Javascript/cva.js
@@ -2,7 +2,18 @@ window.addEventListener("keypress", checkKeyPressed, false);
  
 function checkKeyPressed(e) {
     if (e.charCode === 97) {
-        debugger;
         alert("The 'a' key is pressed.");
     }
 }
+
+(function toggleHumanCow1() {
+    $("#player1").toggleClass("human");
+    $("#player1").toggleClass("cow");
+    setTimeout(toggleHumanCow1, Math.floor((Math.random() * 2000) + 1000));
+})();
+
+(function toggleHumanCow2() {
+    $("#player2").toggleClass("human");
+    $("#player2").toggleClass("cow");
+    setTimeout(toggleHumanCow2, Math.floor((Math.random() * 2000) + 1000));
+})();
