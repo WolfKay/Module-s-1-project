67dac440e8aa53657aa1ceef92ea6b43f4d2b6ce
WolfKay <haroboros@gmail.com>
2017-05-29 01:44:19 +0200

    Button a keydown finally prints ship position and runs an if else statement when position is not parallel to the player

--- Javascript/cvaGameControls.js
+++ Javascript/cvaGameControls.js
@@ -1,9 +1,11 @@
+var game;
+
 window.addEventListener("keypress", checkKeyPressed, false);
 
 function checkKeyPressed(e) {
     if (e.charCode === 97) {
-        CheckPosition(player1Selector);
-        CheckPosition.triangulator();
+        game = new CheckPosition(player1Selector);
+        game.triangulator();
 
     }
 
--- Javascript/cvaGameLogic.js
+++ Javascript/cvaGameLogic.js
@@ -1,18 +1,16 @@
-// this part is to find out the elemnets position within it's parent (the game board)
-// var shipSelector;
+
 var player1Selector = $("#player1").offset();
-// var player2Selector = $("#player2").offset();
+var player2Selector = $("#player2").offset();
 
 function CheckPosition(player) {
     this.player = player;
     this.shipSelector;
     this.shipSelector = $(".space-ship").offset();
-    // this.player1Position = player1Selector.left;
-    // this.player2Position = player2Selector.left;
+
     console.log(this.shipSelector);
 
     CheckPosition.prototype.triangulator = function () {
-        shipPosition = this.shipSelector;
+        this.shipPosition = this.shipSelector;
         if (this.player >= this.shipPosition - 10 && this.player <= this.shipPosition + 10) {
             console.log("a");
         }
@@ -22,7 +20,6 @@ function CheckPosition(player) {
     };
 };
 
-// var game = new CheckPosition();
 
 
 
