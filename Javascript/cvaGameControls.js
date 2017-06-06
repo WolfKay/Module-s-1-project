734fbaa7c5d339778b57971cc5c39f30b2af9488
Karla Lobo <kay@Karlas-MacBook-Pro.local>
2017-06-06 21:01:11 +0200

    timer workkksssss

--- Javascript/cvaGameControls.js
+++ Javascript/cvaGameControls.js
@@ -1,29 +1,26 @@
-window.addEventListener("keydown", function(e) {
-        var game = new CheckPosition();
+window.addEventListener("keydown", function (e) {
+    var game = new CheckPosition();
 
-        if (e.keyCode === 87) {
-            game.triangulatorP1();
-            $("#p1-score").html(p1Score);
-            
-        } else if (e.keyCode === 38) {
-            game.triangulatorP2();
-            $("#p2-score").html(p2Score);
-        }
-        else if (e.keyCode === 68) {
-            game.moveP1("right");
-        }
-        else if (e.keyCode === 65) {
-            game.moveP1("left");
-        }
-        else if (e.keyCode === 39) {
-            game.moveP2("right");
-        }
-        else if (e.keyCode === 37) {
-            game.moveP2("left");
-        }
-        else if (e.keyCode === 32) {
-            $(".welcome").toggle();
-        }
+    if (e.keyCode === 87) {
+        game.triangulatorP1();
+        $("#p1-score").html(p1Score);
+
+    } else if (e.keyCode === 38) {
+        game.triangulatorP2();
+        $("#p2-score").html(p2Score);
+    } else if (e.keyCode === 68) {
+        game.moveP1("right");
+
+    } else if (e.keyCode === 65) {
+        game.moveP1("left");
+    } else if (e.keyCode === 39) {
+        game.moveP2("right");
+    } else if (e.keyCode === 37) {
+        game.moveP2("left");
+    } else if (e.keyCode === 32) {
+        $(".welcome").toggle();
+        $(".game-pause").toggle();
+    }
 });
 
 
--- Javascript/cvaVisualLogic.js
+++ Javascript/cvaVisualLogic.js
@@ -9,3 +9,15 @@
     $("#player2").toggleClass("cow");
     setTimeout(toggleHumanCow2, Math.floor((Math.random() * 2000) + 1000));
 })();
+
+function startTimer(time) {
+  document.getElementById('timer').innerHTML = --time;
+  
+  if (time > 0) {
+  	setTimeout(function() { startTimer(time) }, 1000);
+  } else if (){
+
+  }
+}
+
+startTimer(6);
