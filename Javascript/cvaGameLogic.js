ac4c4dc68846e4b3de929602566e12484947f4fe
Karla Lobo <kay@Karlas-MacBook-Pro.local>
2017-06-10 14:48:24 +0200

    Entrega de proyecto sabado 10 Junio

--- Javascript/cvaGameLogic.js
+++ Javascript/cvaGameLogic.js
@@ -27,14 +27,14 @@ function CheckPosition() {
 
   CheckPosition.prototype.triangulatorP1 = function () {
     if (this.player1PositionX >= this.shipPosition - 100 && this.player1PositionX <= this.shipPosition + 100 && $("#player1").hasClass("cow")) {
-      p1Score += 200;
+      p1Score += 500;
       $("#p1-score").css("animation-play-state", "running");
       setTimeout(function(){
         $("#p1-score").css("animation-play-state", "paused");
-      }, 700);
+      }, 500);
 
       $("#player1").animate({
-        bottom: "200"
+        bottom: "100"
       }, 200);
       $("#player1").animate({
         bottom: "20"
@@ -60,14 +60,14 @@ function CheckPosition() {
 
   CheckPosition.prototype.triangulatorP2 = function () {
     if (this.player2PositionX >= this.shipPosition - 100 && this.player2PositionX <= this.shipPosition + 100 && $("#player2").hasClass("cow")) {
-      p2Score += 200;
+      p2Score += 500;
       $("#p2-score").css("animation-play-state", "running");
       setTimeout(function(){
         $("#p2-score").css("animation-play-state", "paused");
       }, 700);
 
       $("#player2").animate({
-        bottom: "200"
+        bottom: "100"
       }, 200);
       $("#player2").animate({
         bottom: "20"
@@ -91,17 +91,17 @@ function CheckPosition() {
 
   CheckPosition.prototype.moveP1 = function (direction) {
     if (direction === "right" && document.getElementById("player1").offsetLeft < 1100) {
-      document.getElementById("player1").style.left = document.getElementById("player1").offsetLeft + 100 + 'px';
+      document.getElementById("player1").style.left = document.getElementById("player1").offsetLeft + 150 + 'px';
     } else if (direction === "left" && document.getElementById("player1").offsetLeft > 50) {
-      document.getElementById("player1").style.left = document.getElementById("player1").offsetLeft - 100 + 'px';
+      document.getElementById("player1").style.left = document.getElementById("player1").offsetLeft - 150 + 'px';
     }
   }
 
   CheckPosition.prototype.moveP2 = function (direction) {
     if (direction === "right" && document.getElementById("player2").offsetLeft < 1100) {
-      document.getElementById("player2").style.left = document.getElementById("player2").offsetLeft + 100 + 'px';
+      document.getElementById("player2").style.left = document.getElementById("player2").offsetLeft + 150 + 'px';
     } else if (direction === "left" && document.getElementById("player2").offsetLeft > 50) {
-      document.getElementById("player2").style.left = document.getElementById("player2").offsetLeft - 100 + 'px';
+      document.getElementById("player2").style.left = document.getElementById("player2").offsetLeft - 150 + 'px';
     }
 
   }
